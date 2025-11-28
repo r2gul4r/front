1df5e4826b9f512e4184f5ca33b5d121a8dfdeed
r2gul4r <leflex0821@gmail.com>
2025-11-28 09:16:52 +0900

    12

--- js/counter.js
+++ js/counter.js
@@ -1,48 +1,43 @@
-        $(function () {
+$(function () {
+    function runCounter($counter) {
+        var target = parseInt($counter.attr("data-target")),
+            count = 0,
+            speed = 50;
 
-            function runCounter($counter) {
-                var target = parseInt($counter.attr("data-target")),
-                    count = 0,
-                    speed = 50;
+        $counter.text(0);
 
-                $counter.text(0);
+        var counterInterval = setInterval(function () {
+            count++;
+            if (count >= target) {
+                clearInterval(counterInterval);
+            }
+            $counter.text(count);
+        }, speed);
+    } // function runCounter($counter) end
 
-                var counterInterval = setInterval(function () {
-                    // count += Math.ceil(target / 100);
-                    count++;
-                    if (count >= target) {
-                        //count = target;
-                        clearInterval(counterInterval);
-                    }
-                    $counter.text(count);
-                }, speed);
-            } // function runCounter($counter) end
+    function checkScroll() {
+        const scrollTop = $(window).scrollTop(),
+            windowHeight = $(window).height(),
+            elementTop = $("#counters").offset().top;
 
-            function checkScroll() {
-                const scrollTop = $(window).scrollTop(),
-                    windowHeight = $(window).height(),
-                    elementTop = $('#counters').offset().top;
+        $(".counter").each(function () {
+            const $this = $(this);
+            //console.log($this);
 
-                $(".counter").each(function () {
-                    const $this = $(this);
-
-                    if (scrollTop + windowHeight > elementTop + 100) {
-                    // if (scrollTop + windowHeight > elementTop + 50 && scrollTop < elementTop + $this.outerHeight()) {
-                        if (!$this.hasClass("active")) {
-                            $this.addClass("active");
-                            runCounter($this);
-                        }
-                    } else {
-                        $this.removeClass("active");
-                        $this.text(0);
-                    }
-                });
-                console.log($(window).scrollTop());
-            } // function checkScroll() end
-
-            // $.throttle(시간, 함수)
-            // -> 시간은 ms, 예) 100 -> 0.1초
-            $(window).on("scroll", $.throttle(100, checkScroll));
-            //$(window).on("scroll", checkScroll);
-            checkScroll();
+            if (scrollTop + windowHeight > elementTop + 100) {
+                if (!$this.hasClass("active")) {
+                    $this.addClass("active");
+                    runCounter($this);
+                }
+            } else {
+                $this.removeClass("active");
+                $this.text(0);
+            }
         });
+        //console.log($(window).scrollTop());
+    } // function checkScroll() end
+
+    $(window).on("scroll", $.throttle(100, checkScroll));
+    checkScroll();
+
+}); // $(document).ready() end
